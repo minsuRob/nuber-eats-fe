ba58a9c5dcede06fcc5ced6123eab6190677e73e
minsu.lee <minsu.rob@gmail.com>
2024-09-07 22:12:34 +0900

    18.0: verify 요청과 인증 css 처리

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import nuberLogo from "../images/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useMe } from "../hooks/useMe";
 
-export const Header = () => (
-  <header className="py-4">
-    <div className="w-full xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-      <img src={nuberLogo} className="w-24" alt="Nuber Eats" />
-      <span className="text-xs">
-        <FontAwesomeIcon icon={faUser} className="text-xl" />
-      </span>
-    </div>
-  </header>
-);
+export const Header = () => {
+  const { data } = useMe();
+  return (
+    <>
+      {!data?.me.verified && (
+        <div className="bg-red-500 p-3 text-center text-xs text-white">
+          <span>Please verify your email</span>
+        </div>
+      )}
+      <header className="py-4">
+        <div className="w-full xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
+          <img src={nuberLogo} className="w-24" alt="Nuber Eats" />
+          <span className="text-xs">
+            <FontAwesomeIcon icon={faUser} className="text-xl" />
+          </span>
+        </div>
+      </header>
+    </>
+  );
+};
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,35 +1,39 @@
 import React from "react";
-
-import { isLoggedInVar } from "../apollo";
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from "react-router-dom";
-import { Restaurants } from "../pages/client/restaurants";
 import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
+import { Restaurants } from "../pages/client/restaurants";
+import { ConfirmEmail } from "../pages/user/confirm-email";
 
 const ClientRoutes = [
-  <Route path="/" exact>
+  <Route key={1} path="/" exact>
     <Restaurants />
+  </Route>,
+  <Route key={2} path="/confirm" exact>
+    <ConfirmEmail />
   </Route>,
 ];
 
 export const LoggedInRouter = () => {
   const { data, loading, error } = useMe();
-  /*if (!data || loading || error) {
+
+  if (!data || loading || error) {
     return (
-      <div className="justify-center h-screen items-center flex">
+      <div className="h-screen flex justify-center items-center">
         <span className="font-medium text-xl tracking-wide">Loading...</span>
       </div>
     );
-  }*/
+  }
   return (
     <Router>
       <Header />
       <Switch>
+        {data?.me.role === "Client" && ClientRoutes}
         <Redirect to="/" />
       </Switch>
     </Router>
