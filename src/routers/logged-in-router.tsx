b659d0d010ab563e589b4d2847a7d9e78f5fd6b5
minsu.lee <minsu.rob@gmail.com>
2024-09-07 19:36:27 +0900

    17.15: jwt를 넣은 authlink 추가, 로그인 기본 화면 추가

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 
 import { isLoggedInVar } from "../apollo";
-
-export const LoggedInRouter = () => (
-  <div>
-    <h1>Logged In</h1>
-    <button onClick={() => isLoggedInVar(false)}>Log Out</button>
-  </div>
-);
+import { gql, useQuery } from "@apollo/client";
+import { meQuery } from "../__generated__/meQuery";
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  if (!data || loading || error) {
+    return (
+      <div className="justify-center h-screen items-center flex">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{data.me.email}</h1>
+    </div>
+  );
+};
