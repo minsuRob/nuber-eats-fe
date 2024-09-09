1d0763db2b393b6567bfdde0dfbfcfcea9265652
minsu.lee <minsu.rob@gmail.com>
2024-09-10 01:09:29 +0900

    19.2: 레스토랑 grid 열람 추가

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,9 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import {
-  restaurantsPageQuery,
-  restaurantsPageQueryVariables,
-} from "../../__generated__/restaurantsPageQuery";
+import { restaurantsPageQuery } from "../../__generated__/restaurantsPageQuery";
 
 const RESTAURANTS_QUERY = gql`
   query restaurantsPageQuery {
@@ -18,33 +15,28 @@ const RESTAURANTS_QUERY = gql`
         restaurantCount
       }
     }
-  }
-`;
-
-/*
-$input: RestaurantsInput!
-    restaurants(input: $input) {
+    restaurants {
       ok
       error
       totalPages
       totalResults
+      results {
+        id
+        name
+        coverImg
+        category {
+          name
+        }
+        address
+      }
     }
-  results {
-    id
-    name
-    coverImg
-    category {
-      name
-    }
-    address
-    isPromoted
-  }*/
+  }
+`;
 
 export const Restaurants = () => {
-  const { data, loading } = useQuery<
-    restaurantsPageQuery,
-    restaurantsPageQueryVariables
-  >(RESTAURANTS_QUERY);
+  const { data, loading } = useQuery<restaurantsPageQuery>(RESTAURANTS_QUERY);
+  console.log(data?.restaurants);
+
   return (
     <div>
       <form className="bg-gray-800 w-full py-40 items-center flex justify-center">
@@ -57,18 +49,31 @@ export const Restaurants = () => {
       {/* 
         <div><div>
         )} */}
-
       {!loading && (
         <div className="max-w-screen-2xl mx-auto mt-8">
           <div className="flex justify-around max-w-sm mx-auto">
             {data?.allCategories.categories?.map((category) => (
-              <div className="flex flex-col items-center cursor-pointer">
+              <div className="flex flex-col group items-center cursor-pointer">
                 <div
-                  className="w-14 h-14 bg-color hover:bg-gray-100 rounded-full bg-cover"
+                  className="w-16 h-16 bg-color group-hover:bg-gray-100 rounded-full bg-cover"
                   style={{ backgroundImage: `url(${category.coverImg})` }}
                 ></div>
                 <span className="mt-1 text-sm text-center font-medium">
                   {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className="grid grid-cols-3 gap-x-5 gap-y-10 mt-10">
+            {data?.restaurants.results?.map((restaurant) => (
+              <div>
+                <div
+                  style={{ backgroundImage: `url(${restaurant.coverImg})` }}
+                  className="bg-red-500 bg-cover bg-center mb-3 py-28"
+                ></div>
+                <h3 className="text-xl font-medium">{restaurant.name}</h3>
+                <span className="border-t-2 border-gray-200">
+                  {restaurant.category?.name}
                 </span>
               </div>
             ))}
